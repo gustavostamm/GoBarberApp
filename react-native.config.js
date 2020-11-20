e55bef9c508f7734730f5e46f061f57bb1e29b24
Gustavo Stamm <gustavo.stamm@gmail.com>
2020-11-20 13:15:48 -0300

    Structure and patterns

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    project: {
+        ios:{},
+        android: {},
+    },
+    assets: [
+        './assets/fonts/'
+    ],
+
+}
+
+//yarn react-native link -> need to run this code to link our fonts to our project
