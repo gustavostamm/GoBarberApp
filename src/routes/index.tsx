18900eda323a6c1e28f6100e94c052925573699a
Gustavo Stamm <gustavo.stamm@gmail.com>
2020-11-22 13:08:36 -0300

    Authentication and registration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { create } from 'react-test-renderer';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SingUp';
@@ -11,10 +10,11 @@ const AuthRoutes: React.FC = () => (
     <Auth.Navigator
         screenOptions={{
             headerShown: false,
-            cardStyle: { backgroundColor: '#312e38'},
-        }}>
+            cardStyle: { backgroundColor: '#312e38' },
+        }}
+    >
         <Auth.Screen name="SignIn" component={SignIn} />
-        <Auth.Screen name="SignUp" component={SignUp} />
+        <Auth.Screen name="SignUp" component={SignUp} />        
     </Auth.Navigator>
 );
 
